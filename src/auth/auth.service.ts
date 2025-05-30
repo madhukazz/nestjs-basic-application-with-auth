fe0759dc8b5504c65d57ff9417856796f53986aa
Madhuka Dilshan <madhukadilshan8@gmail.com>
2025-05-30 19:06:17 +0530

    Implement JWT authentication with user login and token generation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcrypt';
 import { UserService } from 'src/user/user.service';
+import { AuthJwtPayload } from './types/auth.jwtpayload';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private userService: UserService) {}
+    constructor(private userService: UserService, private jwtService: JwtService) {}
 
     async validateUser(email: string, password: string) {
         const user = await this.userService.findByEmail(email);
@@ -13,6 +15,11 @@ export class AuthService {
         const isPasswordMatch = await compare(password, user.password);
         if(!isPasswordMatch) throw new UnauthorizedException("Invalid credentials");
 
-        return { id: user.id }; 
+        return { id: user.id };
+    }
+
+    login(userId: number){
+        const payload: AuthJwtPayload = { sub : userId } 
+        return this.jwtService.sign(payload);
     }
 }

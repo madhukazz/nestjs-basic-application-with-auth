26dae13d5e04a34b59dae834479bee287632a2fa
Madhuka Dilshan <madhukadilshan8@gmail.com>
2025-06-04 12:20:31 +0530

    Add JWT authentication strategy and guard; refactor user service and controller for profile retrieval

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcrypt';
 import { UserService } from 'src/user/user.service';
-import { AuthJwtPayload } from './types/auth.jwtpayload';
+import { AuthJwtPayload } from './types/auth.jwtPayload';
 
 @Injectable()
 export class AuthService {
